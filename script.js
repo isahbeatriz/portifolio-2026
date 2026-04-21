5ffdd1de14a8983e579728758860c943067bb4f8
isahbeatriz <isabelly.nunes.rosa@escola.pr.gov.br>
2026-04-20 21:22:24 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Efeito sonoro estilo jogo ao clicar nos links
-const links = document.querySelectorAll('a');
-
-links.forEach(link => {
-  link.addEventListener('click', () => {
-    const audio = new Audio('https://actions.google.com/sounds/v1/cartoon/wood_plank_flicks.ogg');
-    audio.play();
-  });
-});
